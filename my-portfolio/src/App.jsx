652a903bdceb8c2a50b4e9128777479f5c05a95c
Alain Cheng <104754302+alain-cheng@users.noreply.github.com>
2024-09-17 15:48:08 +0800

    add home page

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import About from "./pages/About";
 import Contacts from "./pages/Contacts";
 import Projects from "./pages/Projects";
 
+import headerImg from "./assets/header_img.png";
+import github from  "./assets/github-icon.png";
+import linkedin from "./assets/linkedin-icon.png";
+import instagram from "./assets/instagram-icon.png";
+import mail from "./assets/mail-icon.png";
 
 import './styles/App.css';
 
@@ -12,25 +18,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li><Link to="/">About Me</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contacts">Contacts</Link></li>
-              </ul>
-            </nav>
-            <Routes>
-              <Route path="/" element={<About/>}/>
-              <Route path="/projects" element={<Projects/>}/>
-              <Route path="/contacts" element={<Contacts/>}/>
-            </Routes>
+        <div className="Header-text">
+          <div className="Ht-main">
+            Hello Im Alain!
           </div>
-        </Router>
+          <div className="Ht-sub">
+            an Undergraduate Student
+          </div>
+          <div className="Ht-desc">
+            I major in Computer Science, specializing in <br/>
+            Software Technology. I am also studying at <br/>
+            De La Salle University, Manila, Philippines.
+          </div>
+        </div>
+        <div className="Header-image">
+          <img src={headerImg} alt="Image"/>
+        </div>
       </header>
-
-      
+      <div className="Socials-group">
+        <img src={github} alt="Github"/>
+        <img src={linkedin} alt="LinkedIn"/>
+        <img src={instagram} alt="Instagram"/>
+        <img src={mail} alt="Mail"/>
+      </div>
     </div>
   );
 }
